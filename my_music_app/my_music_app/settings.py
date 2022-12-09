a742792142aa5a06fe63317afa4283cedc7bcbe8
Aneliya Mavrodieva <118438408+AMavrodieva@users.noreply.github.com>
2022-12-09 11:00:18 +0200

    Update settings.py

--- my_music_app/my_music_app/settings.py
+++ my_music_app/my_music_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-k^9-_0i&mc*w!w%&*z)_gfgr#yo6slco!8wvhzo5($e1u&i62w"
+SECRET_KEY = "purple-unicorn"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
